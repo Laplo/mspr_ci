7daa5ba0b8a3fc6cd183633af145f30e8d741648
Laplo <ronanlaplaud4@gmail.com>
2020-01-20 19:33:30 +0100

    start front

--- src/Server.ts
+++ src/Server.ts
@@ -7,6 +7,8 @@ import container from './shared/Container';
 import * as swagger from 'swagger-express-ts';
 import * as bodyParser from 'body-parser';
 import {globalInfoLogger} from '@shared';
+import * as path from 'path';
+import { Request, Response } from 'express';
 
 const server = new InversifyExpressServer(container);
 server.setConfig((appConfig: any) => {
@@ -29,6 +31,13 @@ server.setConfig((appConfig: any) => {
             path: '/api-docs/highlander/swagger.json',
         },
     ));
+    const viewsDir = path.join(__dirname, 'views');
+    appConfig.set('views', viewsDir);
+    const staticDir = path.join(__dirname, 'public');
+    appConfig.use(express.static(staticDir));
+    appConfig.get('*', (req: Request, res: Response) => {
+        return res.sendFile('index.html', {root: viewsDir});
+    });
 });
 
 server.setErrorConfig((appErr: any) => {
