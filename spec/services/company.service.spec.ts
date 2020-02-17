f0e12ad3fdbae35ea6b8786902fc26dc05b9f59a
Laplo <ronanlaplaud4@gmail.com>
2020-02-17 09:17:43 +0100

    test covergae

--- spec/services/company.service.spec.ts
+++ spec/services/company.service.spec.ts
@@ -1,10 +1,10 @@
-import {calculateTax} from "../../src/shared/CalculateTax";
+import {calculateTax} from '../../src/shared/CalculateTax';
 
 describe('company total revenue', () => {
    it('should return the correct object of ITotalRevenue with correct parameter', () => {
        const objectTax = calculateTax(100);
-        expect(objectTax.includingTax).toEqual(120);
-        expect(objectTax.excludingTax).toEqual(100);
+       expect(objectTax.includingTax).toEqual(120);
+       expect(objectTax.excludingTax).toEqual(100);
     });
 
    it('should throw an Error because of bad parameter', () => {
